8fbc1214db9b4e1bb79cb800d3a974484a6ec162
Jonatas Santos <jonatas_santos3161@hotmail.com>
2022-10-16 02:32:42 -0300

    update flutter version and add launch icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GreenGrocer',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
