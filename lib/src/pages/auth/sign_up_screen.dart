cd4d203cd54768ba086a8e76162dcbb6c79450f7
Jonatas Santos <ext.jonatas.santos@vocedm.com.br>
2023-07-29 21:15:07 -0300

    clean code signup page

--- lib/src/pages/auth/sign_up_screen.dart
+++ lib/src/pages/auth/sign_up_screen.dart
@@ -15,84 +15,100 @@ class SignUpScreen extends StatelessWidget {
         child: SizedBox(
           height: size.height,
           width: size.width,
-          child: Stack(
-            children: [
-              Column(
-                children: [
-                  const Expanded(
-                    child: Center(
-                      child: Text(
-                        'Cadastro',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 35,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(45),
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const CustomTextField(icon: Icons.email, label: 'E-mail'),
-                        const CustomTextField(
-                          icon: Icons.lock,
-                          label: 'Senha',
-                          isSecret: true,
-                        ),
-                        const CustomTextField(icon: Icons.person, label: 'Nome'),
-                        CustomTextField(
-                          icon: Icons.phone,
-                          label: 'Celular',
-                          inputFormatters: [UtilsServices().phoneFormatter],
-                        ),
-                        CustomTextField(
-                          icon: Icons.file_copy,
-                          label: 'CPF',
-                          inputFormatters: [UtilsServices().cpfFormatter],
-                        ),
-                        SizedBox(
-                          height: 50,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18),
-                              ),
-                            ),
-                            onPressed: () {},
-                            child: const Text(
-                              'Cadastrar Usuário',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Positioned(
-                top: 10,
-                left: 10,
-                child: SafeArea(
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: const Icon(Icons.arrow_back_ios),
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
+          child: _buildSignIn(context),
+        ),
+      ),
+    );
+  }
+
+  Stack _buildSignIn(BuildContext context) {
+    return Stack(
+      children: [
+        Column(
+          children: [
+            _buildHeader(),
+            _buildForm(),
+          ],
+        ),
+        Positioned(
+          top: 10,
+          left: 10,
+          child: SafeArea(
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(Icons.arrow_back_ios),
+              color: Colors.white,
+            ),
           ),
+        ),
+      ],
+    );
+  }
+
+  Expanded _buildHeader() {
+    return const Expanded(
+      child: Center(
+        child: Text(
+          'Cadastro',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 35,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container _buildForm() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(45),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const CustomTextField(icon: Icons.email, label: 'E-mail'),
+          const CustomTextField(
+            icon: Icons.lock,
+            label: 'Senha',
+            isSecret: true,
+          ),
+          const CustomTextField(icon: Icons.person, label: 'Nome'),
+          CustomTextField(
+            icon: Icons.phone,
+            label: 'Celular',
+            inputFormatters: [UtilsServices().phoneFormatter],
+          ),
+          CustomTextField(
+            icon: Icons.file_copy,
+            label: 'CPF',
+            inputFormatters: [UtilsServices().cpfFormatter],
+          ),
+          _registerbutton()
+        ],
+      ),
+    );
+  }
+
+  SizedBox _registerbutton() {
+    return SizedBox(
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+        ),
+        onPressed: () {},
+        child: const Text(
+          'Cadastrar Usuário',
+          style: TextStyle(fontSize: 18),
         ),
       ),
     );
